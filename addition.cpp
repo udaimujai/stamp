a00e8e48ba54193396bc7b9c987a28c638568c4f
Udayakumar Harigopal <udaimujai@yahoo.co.in>
2021-03-10 00:37:48 +0100

    Added Addition files

---
+++ addition.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    int b;
+    cout << "Enter a number" << endl;
+    cin >> a;
+    cout << "enter a number" << endl;
+    cin >> b;
+    int sum = a + b;
+    cout << "sum of the integer is" << sum << endl;
+    return 0;
+}
